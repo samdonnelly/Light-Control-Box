c8f2c7b4d081c896b9edbafeb1d5af579d219fb8
Sam Donnelly <samueldonnelly11@gmail.com>
2021-11-09 21:27:21 -0800

    Reformatted file. Added comments for clarity.

--- include/main.h
+++ include/main.h
@@ -1,6 +1,6 @@
 /*
- *
- *
+ * Header file for main.c 
+ * Add defines go here. 
  */
 
 // Include libraries 
@@ -11,31 +11,31 @@
 #define MAIN_H
 
 // Setup constants
-#define BAUD     115200  // Baud rate of 9600 bps 
-#define TIME     2       // Serial timeout time 
-#define BUF_SIZE 64      // Adruino Serial.available buf size if 64 bytes
+#define BAUD     115200         // Baud rate of 9600 bps 
+#define TIME     2              // Serial timeout time 
+#define BUF_SIZE 64             // Adruino Serial.available buf size if 64 bytes
 
 // Pin assignments
-#define button0_pin  2   // Button 0: Pin 2
-#define button1_pin  4   // Button 1: Pin 4
-#define button2_pin  7   // Button 2: Pin 7
-#define button3_pin  8   // Button 3: Pin 8
-#define lightstring0 6   // Light Sequence 0: Pin 6
-#define lightstring1 9   // Light Sequence 1: Pin 9
-#define lightstring2 10  // Light Sequence 2: Pin 10
-#define lightstring3 11  // Light Sequence 3: Pin 11
+#define button0_pin  2          // Button 0: Pin 2
+#define button1_pin  4          // Button 1: Pin 4
+#define button2_pin  7          // Button 2: Pin 7
+#define button3_pin  8          // Button 3: Pin 8
+#define lightstring0 6          // Light Sequence 0: Pin 6
+#define lightstring1 9          // Light Sequence 1: Pin 9
+#define lightstring2 10         // Light Sequence 2: Pin 10
+#define lightstring3 11         // Light Sequence 3: Pin 11
 
 // EEPROM Addresses
-#define EEPROM0 0  // EEPROM address 1
-#define EEPROM1 1  // EEPROM address 2
-#define EEPROM2 2  // EEPROM address 3
-#define EEPROM3 3  // EEPROM address 4
-#define EEPROM4 4  // EEPROM address 5 
+#define EEPROM0 0               // EEPROM address 1
+#define EEPROM1 1               // EEPROM address 2
+#define EEPROM2 2               // EEPROM address 3
+#define EEPROM3 3               // EEPROM address 4
+#define EEPROM4 4               // EEPROM address 5 
 
 // State machine sizes 
-#define NUM_TRANS_MAIN    6   // Number of transitions in main state machine 
-#define NUM_STATES_MAIN   6   // Number of states in main state machine 
-#define NUM_STATES_LIGHTS 20  // Number of states in light sequence state machine 
+#define NUM_TRANS_MAIN    6     // Number of transitions in main state machine 
+#define NUM_STATES_MAIN   6     // Number of states in main state machine 
+#define NUM_STATES_LIGHTS 20    // Number of states in light sequence state machine 
 
 // Main state machine states 
 #define button0_state 0
@@ -46,16 +46,54 @@
 #define program_state 5
 
 // Delays 
-#define DELAY_B 0  // Delay for switching between button states 
-#define DELAY_L 0  // Delay after each light sequence state iteration 
+#define DELAY_B 0               // Delay for switching between button states 
+#define DELAY_L 0               // Delay after each light sequence state iteration 
 
 // Function B0
-#define T_B0 85000  // Oscillation period of the combined light sequence. Make an adjustable EEPROM.
+#define T_B0 85000              // Oscillation period of the combined light sequence. 
+                                // Make an adjustable EEPROM.
+
+// Function B1
+
+// Function B2
+
+// Function B3
+
+// Function B4
+
+// Function B5
+
+// Function B6
+
+// Function B7
+
+// Function B8
+
+// Function B9
 
 // Function C0
 #define T_C0   500000
 #define T_C0_W 1000
 #define T_C0_F 750
+
+// Function C1
+
+// Function C2
+
+// Function C3
+
+// Function C4
+
+// Function C5
+
+// Function C6
+
+// Function C7
+
+// Function C8
+
+// Function C9
+
 
 #endif /* MAIN_FUNCTIONS_H */
 
